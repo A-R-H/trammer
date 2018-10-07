e9e8fe99f99ae53488e1d55489cf0fc3c85890b0
alex <alex.r.hancock@googlemail.com>
2018-10-07 16:39:24 +0100

    envs

--- src/services/TramTimeServices.js
+++ src/services/TramTimeServices.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 let uri = process.env.URI;
+const DB_URL = process.env.DB_URL;
 
-// if (!uri) {
-//   uri = require("../config");
-// }
+if (!uri) {
+  uri = process.env.DB_URL || require("../config");
+}
+
+// uri = require("../config");
 
 const Get = params => {
   return axios.get(`${uri}/api/tram`);
