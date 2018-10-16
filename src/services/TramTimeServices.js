6aaba69c243e3cd65834b045e1a2b16ca9b86d5e
alex <alex.r.hancock@googlemail.com>
2018-10-16 22:17:40 +0100

    target deployed backend

--- src/services/TramTimeServices.js
+++ src/services/TramTimeServices.js
@@ -2,8 +2,8 @@ import axios from "axios";
 // const uri = process.env.ocp || require("../config");
 
 const Get = () => {
-  // return axios.get(`https://trammer-back.herokuapp.com/api/tram`);
-  return axios.get(`http://localhost:8080/api/tram`);
+  // return axios.get(`http://localhost:8080/api/tram`);
+  return axios.get(`https://trammer-back.herokuapp.com/api/tram`);
 };
 
 export default { Get };
